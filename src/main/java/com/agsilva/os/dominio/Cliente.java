1ee5942ee3035a02b0b1989b379c2775e947d68f
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2024-09-25 22:34:26 -0300

    Criação da classe pessoa, técnico e cliente

---
+++ src/main/java/com/agsilva/os/dominio/Cliente.java
@@ -0,0 +1,10 @@
+package com.agsilva.os.dominio;
+
+public class Cliente extends Pessoa{
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome, String cpf, String telefone) {
+        super(id, nome, cpf, telefone);
+    }
+}
